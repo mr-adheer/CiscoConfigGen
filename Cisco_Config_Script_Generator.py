78f4a21c12d7cc669f33267b6af5ae61ecbfd3fd
Adheer <adheer1@gmail.com>
2024-11-07 20:13:27 +0530

    3rdcommid

--- Cisco_Config_Script_Generator.py
+++ Cisco_Config_Script_Generator.py
@@ -2,17 +2,12 @@
 # based on https://developer.cisco.com/codeexchange/github/repo/Tes3awy/Cisco-Configuration-Using-Python-Jinja-CSV/
 ## install the follwoing Python3 packages ##
 # Jinja2
-# Cerberus
-# termcolor
-# colorama
 
 import os
 import time
 from csv import DictReader
 from datetime import datetime
-from colorama import init
 from jinja2 import Environment, FileSystemLoader
-from termcolor import cprint
 
 CONFIG_TEMPLATE = "Config_Script_Template.j2"
 OUTPUT_DIR = "Configuration_Output_Files"
@@ -21,10 +16,10 @@ INPUT_CSV_FILE = "Config_Script_Input_Values.csv"
 # Create configs directory if not created already
 time = datetime.now()
 DIR_NOW = f"{time.strftime('%Y%b%d_%H%M%S')}_{'Config_Scripts'}"
-FILE_NOW = time.strftime('%d_%b_%Y-%H:%M:%S')
+FILE_NOW = time.strftime('%d_%b_%Y-%H-%M-%S')
 DIR_PATH = os.path.join(OUTPUT_DIR,DIR_NOW)
 if not os.path.exists(DIR_PATH):
-    os.mkdir(DIR_PATH)
+    os.makedirs(DIR_PATH)
 
 def build_template(config_template_file: str = CONFIG_TEMPLATE, parameters_file: str = INPUT_CSV_FILE,):
     
@@ -38,12 +33,10 @@ def build_template(config_template_file: str = CONFIG_TEMPLATE, parameters_file:
             dict_params = dict(param)
             config = template.render(dict_params)
             global FILE_NOW, DIR_PATH
-            FILE_NAME = f"{dict_params.get('siteid')}_{'('}{dict_params.get('hostname')}{')'}_{FILE_NOW}.{'txt'}"
+            FILE_NAME = f"{dict_params.get('DEVICE_TYPE')}{'_('}{dict_params.get('BRANCH_NAME')}{'-'}{dict_params.get('BRANCH_ID')}{')'}_{'('}{dict_params.get('HOSTNAME')}{')'}_{FILE_NOW}.{'txt'}"
             FILE_PATH = os.path.join(DIR_PATH, FILE_NAME)
             with open(file=FILE_PATH, mode="wt", encoding="utf-8") as cfg_file:
                 cfg_file.write(config)
-            cprint(
-                f"Configuration file '{FILE_NAME}' is created in Folder" '', "green", end="\n\n"
-        )
+            print(f"Configuration file '{FILE_NAME}' is created in Folder '{DIR_NOW}'", end="\n\n")
 if __name__ == "__main__":
     build_template()
